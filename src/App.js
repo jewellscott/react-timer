b415818989a4adecb8e03fce577554852ee2ca0c
Jewell Scott <jewelljrscott@gmail.com>
2022-06-29 19:38:28 -0400

    Add basic timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  function startTimer() {
+    setIsRunning(true);
+  }
+
+  function pauseTimer() {
+    setIsRunning(false);
+  }
+
+  function resetTimer() {
+    setIsRunning(false);
+    setSeconds(0);
+  }
+
+
+  useEffect(() => {
+    let interval = null;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>react-timer</header>
+      <main>
+        <div className="timer">
+          <p>{seconds} seconds</p>
+        </div>
+        <button id="start" onClick={startTimer}>Start</button>
+        <button id="stop" onClick={pauseTimer}>Pause</button>
+        <button it="reset" onClick={resetTimer}>Reset</button>
+      </main>
     </div>
   );
 }
